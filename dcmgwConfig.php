dd622d0cfb2e6600b803ed7ebbee725b4510cb95
jap1968 <josean1968@gmail.com>
2013-05-28 14:10:15 +0200

    dcmgw pacs definition

---
+++ dcmgwConfig.php
@@ -0,0 +1,97 @@
+<?php
+
+/*
+
+    Copyright (C) 2013  Jose Antonio Perez
+    [ http://goo.gl/lW17d ]
+
+    This file is part of dcmgw (Dicom gateway)
+    [ https://github.com/jap1968/dcmgw ]
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see http://www.gnu.org/licenses/gpl.html
+    
+*/
+
+// ******* ********* ********* ********* ********* ********* ********* *********
+
+/**
+ * Avoids direct execution of the script
+ */
+  if (!defined('DCMGW_INCLUDE')) {
+    header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
+    die;
+  }
+
+// ******* ********* ********* ********* ********* ********* ********* *********
+// Configuration: Change values to fit your environment
+// ******* ********* ********* ********* ********* ********* ********* *********
+
+// You should end up having a variable $pacs pointing to your PACS server.
+// You can find below some configuration samples for public servers
+// Please, for testing purposes use your own server (if possible) to avoid overloading the public servers
+
+// http://www.dicomserver.co.uk/
+// http://wado.medicalconnections.co.uk/WADO/WADO.asp
+$pacsMedicalConnections = new PACS('TEST', 'www.dicomserver.co.uk', '11112');
+$pacsMedicalConnections->setWado('http', 'www.dicomserver.co.uk', '80', 'wado/WADO.asp');
+
+// DC's public PixelMed PACS.
+// No WADO?
+$pacsPixelMed = new PACS('AWSPIXELMEDPUB', '184.73.255.26', '11112');
+
+// Public JVSdicom Server. Not sure if this one has any contents.
+// No WADO ?
+// More information: http://153.1.200.58/?q=dicom_images
+$pacsJVSdicom = new PACS('JVSDICOM', '153.1.200.58', '104');
+
+// My own PACS
+$pacsHome = new DCM4CHEE('DCMTEST', '192.168.1.44');
+
+
+$pacs = $pacsHome;
+
+// ******* ********* ********* ********* ********* ********* ********* *********
+
+// This script makes use of dcmqr, from the dcm4che2 Dicom toolkit
+// dcm4che2 Dicom toolkit: http://www.dcm4che.org/confluence/display/d2/dcm4che2+DICOM+Toolkit
+// dcmqr: http://www.dcm4che.org/confluence/display/d2/dcmqr
+define ('PATH_BASE_DCM4CHE2', '/usr/local/dcm4che/dcm4che2/bin/');
+define ('PATH_BASE_DICOM_CACHE', '/tmp/dicom/');
+
+define ('PATH_WGET', '/usr/bin/wget');
+define ('PATH_CURL', '/usr/bin/curl');
+define ('RETRIEVE_WGET', 0);
+define ('RETRIEVE_CURL', 1);
+define ('RETRIEVE_METHOD', RETRIEVE_CURL);
+
+define('SHOW_REQUEST', false);
+define('XML_ENCODING', 'UTF-8');
+
+define ('DEBUG_NONE', 0);
+define ('DEBUG_INFO', 5);
+define ('DEBUG_DUMP', 9);
+define ('DEBUG_LEVEL', DEBUG_NONE);
+
+define ('AETITLE_GATEWAY', 'DCMQR');
+
+// ******* ********* ********* ********* ********* ********* ********* *********
+
+define('QUERY_REQUEST_ROOT', '0');
+define('QUERY_RESPONSE_ROOT', '1');
+define('QUERY_REQUEST', '2');
+define('QUERY_RESPONSE', '3');
+
+// ******* ********* ********* ********* ********* ********* ********* *********
+
+?>
---
+++ dcmgwPacs.php
@@ -0,0 +1,134 @@
+<?php
+
+/*
+
+    Copyright (C) 2013  Jose Antonio Perez
+    [ http://goo.gl/lW17d ]
+
+    This file is part of dcmgw (Dicom gateway)
+    [ https://github.com/jap1968/dcmgw ]
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see http://www.gnu.org/licenses/gpl.html
+    
+*/
+
+// ******* ********* ********* ********* ********* ********* ********* *********
+
+/**
+ * Avoids direct execution of the script
+ */
+  if (!defined('DCMGW_INCLUDE')) {
+    header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
+    die;
+  }
+
+// ******* ********* ********* ********* ********* ********* ********* *********
+// Configuration: Define values for your own PACS
+// ******* ********* ********* ********* ********* ********* ********* *********
+
+class PACS
+{
+  // DICOM setup
+  public $AETitle;
+  public $host;
+  public $port;
+  
+  // WADO setup
+  public $wadoProtocol;
+  public $wadoHost;
+  public $wadoPort;
+  public $wadoScript;
+
+  public $lang = 'en_US'; // No need to change
+  public $encoding = 'iso-8859-1'; // May need to change to UTF-8
+
+  function __construct($AETitle, $host, $port) {
+    $this->AETitle = $AETitle;
+    $this->host = $host;
+    $this->port = $port;
+
+    $this->wadoProtocol = 'http';
+    $this->wadoHost = $host;
+    $this->wadoPort = $port;
+  }
+
+  public function getDicomServer() {
+    $dicomServer = $this->AETitle.'@'.$this->host.':'.$this->port;
+    return $dicomServer;
+  }
+
+  /**
+   * Force Dicom Q/R execution under a given locale to deal properly with non-ascii characters
+   */
+  public function getLocale() {
+    return $this->lang.'.'.$this->encoding;
+  }
+
+  /**
+   * In some environments Wado server runs independently of Dicom server
+   */
+  public function setWado($wadoProtocol, $wadoHost, $wadoPort, $wadoScript) {
+    $this->wadoProtocol = $wadoProtocol;
+    $this->wadoHost = $wadoHost;
+    $this->wadoPort = $wadoPort;
+    $this->wadoScript = $wadoScript;
+  }
+
+  public function setWadoScript($wadoScript) {
+    $this->wadoScript = $wadoScript;
+  }
+
+  public function getUriWado($studyUID, $seriesUID, $objectUID) {
+// Additional parameters ???
+  // contentType
+  // transferSyntax (UID)
+
+    $codeBase = "{$this->wadoProtocol}://{$this->wadoHost}:{$this->wadoPort}/{$this->wadoScript}?";
+    $queryString = "requestType=WADO&studyUID={$studyUID}&seriesUID={$seriesUID}&objectUID={$objectUID}";
+    $uriWado = $codeBase.$queryString;
+    return $uriWado;
+  }
+}
+
+// ******* ********* ********* ********* ********* ********* ********* *********
+
+// Specific subclasses for some popular Dicom servers
+
+class DCM4CHEE extends PACS {
+  function __construct($AETitle, $host) {
+    $port = '11112';
+    parent::__construct($AETitle, $host, $port);
+    $this->wadoPort = '8080';
+    $this->setWadoScript('wado');
+  }
+}
+
+class CONQUEST extends PACS {
+  function __construct($AETitle, $host, $port) {
+    parent::__construct($AETitle, $host, $port);
+    $this->setWadoScript('cgi-bin/dgate.exe');
+  }
+}
+
+class CLEARCANVAS extends PACS {
+  function __construct($AETitle, $host, $port) {
+    parent::__construct($AETitle, $host, $port);
+    $this->wadoPort = '1000';
+    $this->setWadoScript('wado/'.$AETitle);
+  }
+}
+
+// ******* ********* ********* ********* ********* ********* ********* *********
+
+?>
